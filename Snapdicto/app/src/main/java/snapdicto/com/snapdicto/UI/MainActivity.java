0809c8aaf8debcbd8d581f21b8be7c3178bbb2f5
mugabi0978 <powellorenz@gmail.com>
2017-05-26 23:43:06 +0300

    Project Structure and Design: + Initial UI design
    The initial design of the UI was done;-
    
    A revised project structure was also implemented.

--- Snapdicto/app/src/main/java/snapdicto/com/snapdicto/UI/MainActivity.java
+++ Snapdicto/app/src/main/java/snapdicto/com/snapdicto/UI/MainActivity.java
@@ -1,7 +1,9 @@
-package snapdicto.com.snapdicto;
+package snapdicto.com.snapdicto.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import snapdicto.com.snapdicto.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
